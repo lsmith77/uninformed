67270939c7e1bf6405396fa66e61e748f7adf006
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2009-11-04 18:46:23 +0000

    update all models to use the new base class and current schema.yml, added is_imported to excelfile model

--- trunk/lib/form/doctrine/base/BaseExcelFileForm.class.php
+++ trunk/lib/form/doctrine/base/BaseExcelFileForm.class.php
@@ -12,25 +12,27 @@ class BaseExcelFileForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'         => new sfWidgetFormInputHidden(),
-      'name'       => new sfWidgetFormInput(),
-      'tag_id'     => new sfWidgetFormDoctrineChoice(array('model' => 'Tag', 'add_empty' => false)),
-      'author'     => new sfWidgetFormInput(),
-      'file'       => new sfWidgetFormInput(),
-      'import_id'  => new sfWidgetFormDoctrineChoice(array('model' => 'Import', 'add_empty' => true)),
-      'created_at' => new sfWidgetFormDateTime(),
-      'updated_at' => new sfWidgetFormDateTime(),
+      'id'          => new sfWidgetFormInputHidden(),
+      'name'        => new sfWidgetFormInput(),
+      'tag_id'      => new sfWidgetFormDoctrineChoice(array('model' => 'Tag', 'add_empty' => false)),
+      'author'      => new sfWidgetFormInput(),
+      'file'        => new sfWidgetFormInput(),
+      'import_id'   => new sfWidgetFormDoctrineChoice(array('model' => 'Import', 'add_empty' => true)),
+      'is_imported' => new sfWidgetFormInputCheckbox(),
+      'created_at'  => new sfWidgetFormDateTime(),
+      'updated_at'  => new sfWidgetFormDateTime(),
     ));
 
     $this->setValidators(array(
-      'id'         => new sfValidatorDoctrineChoice(array('model' => 'ExcelFile', 'column' => 'id', 'required' => false)),
-      'name'       => new sfValidatorString(array('max_length' => 255)),
-      'tag_id'     => new sfValidatorDoctrineChoice(array('model' => 'Tag')),
-      'author'     => new sfValidatorString(array('max_length' => 255)),
-      'file'       => new sfValidatorString(array('max_length' => 255)),
-      'import_id'  => new sfValidatorDoctrineChoice(array('model' => 'Import', 'required' => false)),
-      'created_at' => new sfValidatorDateTime(array('required' => false)),
-      'updated_at' => new sfValidatorDateTime(array('required' => false)),
+      'id'          => new sfValidatorDoctrineChoice(array('model' => 'ExcelFile', 'column' => 'id', 'required' => false)),
+      'name'        => new sfValidatorString(array('max_length' => 255)),
+      'tag_id'      => new sfValidatorDoctrineChoice(array('model' => 'Tag')),
+      'author'      => new sfValidatorString(array('max_length' => 255)),
+      'file'        => new sfValidatorString(array('max_length' => 255)),
+      'import_id'   => new sfValidatorDoctrineChoice(array('model' => 'Import', 'required' => false)),
+      'is_imported' => new sfValidatorBoolean(array('required' => false)),
+      'created_at'  => new sfValidatorDateTime(array('required' => false)),
+      'updated_at'  => new sfValidatorDateTime(array('required' => false)),
     ));
 
     $this->widgetSchema->setNameFormat('excel_file[%s]');

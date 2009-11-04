b6f9c72653ac1484f796d4c5a3e770c898a92479
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2009-11-04 18:46:23 +0000

    update all models to use the new base class and current schema.yml, added is_imported to excelfile model

--- lib/filter/doctrine/base/BaseExcelFileFormFilter.class.php
+++ lib/filter/doctrine/base/BaseExcelFileFormFilter.class.php
@@ -14,23 +14,25 @@ class BaseExcelFileFormFilter extends BaseFormFilterDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'name'       => new sfWidgetFormFilterInput(),
-      'tag_id'     => new sfWidgetFormDoctrineChoice(array('model' => 'Tag', 'add_empty' => true)),
-      'author'     => new sfWidgetFormFilterInput(),
-      'file'       => new sfWidgetFormFilterInput(),
-      'import_id'  => new sfWidgetFormDoctrineChoice(array('model' => 'Import', 'add_empty' => true)),
-      'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-      'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
+      'name'        => new sfWidgetFormFilterInput(),
+      'tag_id'      => new sfWidgetFormDoctrineChoice(array('model' => 'Tag', 'add_empty' => true)),
+      'author'      => new sfWidgetFormFilterInput(),
+      'file'        => new sfWidgetFormFilterInput(),
+      'import_id'   => new sfWidgetFormDoctrineChoice(array('model' => 'Import', 'add_empty' => true)),
+      'is_imported' => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
+      'created_at'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
+      'updated_at'  => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
     ));
 
     $this->setValidators(array(
-      'name'       => new sfValidatorPass(array('required' => false)),
-      'tag_id'     => new sfValidatorDoctrineChoice(array('required' => false, 'model' => 'Tag', 'column' => 'tag_id')),
-      'author'     => new sfValidatorPass(array('required' => false)),
-      'file'       => new sfValidatorPass(array('required' => false)),
-      'import_id'  => new sfValidatorDoctrineChoice(array('required' => false, 'model' => 'Import', 'column' => 'id')),
-      'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
-      'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
+      'name'        => new sfValidatorPass(array('required' => false)),
+      'tag_id'      => new sfValidatorDoctrineChoice(array('required' => false, 'model' => 'Tag', 'column' => 'tag_id')),
+      'author'      => new sfValidatorPass(array('required' => false)),
+      'file'        => new sfValidatorPass(array('required' => false)),
+      'import_id'   => new sfValidatorDoctrineChoice(array('required' => false, 'model' => 'Import', 'column' => 'id')),
+      'is_imported' => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
+      'created_at'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
+      'updated_at'  => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
     ));
 
     $this->widgetSchema->setNameFormat('excel_file_filters[%s]');
@@ -48,14 +50,15 @@ class BaseExcelFileFormFilter extends BaseFormFilterDoctrine
   public function getFields()
   {
     return array(
-      'id'         => 'Number',
-      'name'       => 'Text',
-      'tag_id'     => 'ForeignKey',
-      'author'     => 'Text',
-      'file'       => 'Text',
-      'import_id'  => 'ForeignKey',
-      'created_at' => 'Date',
-      'updated_at' => 'Date',
+      'id'          => 'Number',
+      'name'        => 'Text',
+      'tag_id'      => 'ForeignKey',
+      'author'      => 'Text',
+      'file'        => 'Text',
+      'import_id'   => 'ForeignKey',
+      'is_imported' => 'Boolean',
+      'created_at'  => 'Date',
+      'updated_at'  => 'Date',
     );
   }
 }
---
+++ lib/form/doctrine/BaseRecordForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * BaseRecord form.
+ *
+ * @package    form
+ * @subpackage BaseRecord
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class BaseRecordForm extends BaseBaseRecordForm
+{
+  public function configure()
+  {
+  }
+}

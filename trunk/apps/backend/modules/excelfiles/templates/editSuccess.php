2b44c4c43365b11f559b510d1caa11720184f956
riedel.dennis@gmail.com <riedel.dennis@gmail.com@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-01-23 20:13:16 +0000

    Initial commit excel files module with file upload.

---
+++ trunk/apps/backend/modules/excelfiles/templates/editSuccess.php
@@ -0,0 +1,3 @@
+<h1>Edit Excel file</h1>
+
+<?php include_partial('form', array('form' => $form)) ?>
---
+++ trunk/apps/backend/modules/excelfiles/templates/newSuccess.php
@@ -0,0 +1,3 @@
+<h1>New Excel file</h1>
+
+<?php include_partial('form', array('form' => $form)) ?>

840cd37f9b563abd5eba3b727a88bff290f79511
riedel.dennis@gmail.com <riedel.dennis@gmail.com@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-02-13 20:45:56 +0000

    Update on excel import. Saves each document, its clause count, document code and followup document. Clause count will be used to add the clauses data for each document as assoziative array.

--- apps/backend/modules/excelfile/lib/uninformed/excelSpreadsheetImport.class.php
+++ apps/backend/modules/excelfile/lib/uninformed/excelSpreadsheetImport.class.php
@@ -2,11 +2,76 @@
 class excelSpreadsheetImport
 { 
   private $excelData = NULL;
+  private $excelStructure = NULL;
+  
+  private static $SHEET = 0;
+  
+  private static $AMOUNT_DOCUMENTCOLUMNS = 27;
+  private static $AMOUNT_CLAUSECOLUMNS = 23;
+  
+  private static $FIRST_DOCUMENTCOLUMN = 2;
+  private static $FIRST_CLAUSECOLUMN = 29;
+  
+  private static $FIRST_ROW = 2;
+  
+  private static $COLUMN_DOCUMENTTITLE = 2;
+  private static $COLUMN_DOCUMENTCODE = 3;
+  private static $COLUMN_DOCUMENTFOLLOWUP = 13;
   
   public function __construct() {}
   
   public function loadDataFromFile($file)
   {
   	$this->excelData = new spreadsheetExcelReader($file);
+  	
+  	$countRows = $this->excelData->rowcount(self::$SHEET);
+  	
+  	$documents = array();
+  	$followups = array();
+    
+  	$title = "";
+  	$code = "";
+  	$followup = "";
+  	
+    for($i = self::$FIRST_ROW; $i <= $countRows; $i++) //rows
+    {
+    	$data = array();
+    	
+    	for($j = self::$FIRST_DOCUMENTCOLUMN; $j < self::$AMOUNT_DOCUMENTCOLUMNS + 1; $j++) //columns
+    	{
+    		$value = trim($this->excelData->value($i,$j,self::$SHEET));
+    		
+    		if($j == self::$COLUMN_DOCUMENTTITLE)
+    		  $title = $value;
+    		else if($j == self::$COLUMN_DOCUMENTCODE)
+          $code = $value;
+        else if($j == self::$COLUMN_DOCUMENTFOLLOWUP)
+          $followup = $value;
+    		else  
+    		  $data[$j] = $value;
+    	}
+    	
+    	if(!isset($documents[$title]))
+        $documents[$title]['countClauses'] = 1;
+      else
+        $documents[$title]['countClauses']++;
+        
+      if(!isset($documents[$title]['code']))
+        $documents[$title]['code'] = $code;
+        
+      if(!isset($documents[$title]['followup']))
+        $documents[$title]['followup'] = $followup;
+        
+      $documents[$title]['data'] = $data;
+    }
+    
+    //add clause data
+    
+    return $documents;
+  }
+  
+  public function saveData()
+  {
+//  	save data in database
   }
 }

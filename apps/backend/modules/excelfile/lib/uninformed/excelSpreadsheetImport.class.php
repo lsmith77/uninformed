7c044ac0ee5d65810d4d99b6aa2bf99ac0509631
riedel.dennis@gmail.com <riedel.dennis@gmail.com@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-02-20 21:19:20 +0000

    Update on Excel Import. Saves document and clause body parents.

--- apps/backend/modules/excelfile/lib/uninformed/excelSpreadsheetImport.class.php
+++ apps/backend/modules/excelfile/lib/uninformed/excelSpreadsheetImport.class.php
@@ -45,7 +45,10 @@ class excelSpreadsheetImport
     	
     	for($j = self::$FIRST_DOCUMENTCOLUMN; $j < self::$FIRST_DOCUMENTCOLUMN + self::$AMOUNT_DOCUMENTCOLUMNS; $j++) //columns
     	{
-    		$value = trim($this->excelData->value($i,$j,self::$SHEET));
+    		if($j == 4) //excel date format data, adoption date column
+    		  $value = trim($this->excelData->raw($i,$j,self::$SHEET));
+    		else
+    		  $value = trim($this->excelData->value($i,$j,self::$SHEET));
     		
     		if($j == self::$COLUMN_DOCUMENTTITLE)
     		  $title = $value;
@@ -106,13 +109,19 @@ class excelSpreadsheetImport
 
   	$clauseHelper = new ClauseHelper();
   	
-    foreach($documents as &$document)
+    foreach($documents as $documentName => &$document)
     {
     	foreach($document['clauses'] as &$clause)
     	{
     		$clauseBody = new ClauseBody();
     		
+    		$clauseNumber = "";
+    		
     		$clauseBody->set('content', $clause[29]);
+    		
+    		$clauseNumber = $clause[30];
+    		$clauseParentNumber = $clause[35];
+    		
     		$clauseBody->set('operative_phrase_id', $clauseHelper->retrieveClauseOperativePhrase($clause[31])); //operative phrase
     		$clauseBody->set('information_type_id', $clauseHelper->retrieveClauseInformationType($clause[32])); //information type
         
@@ -146,32 +155,88 @@ class excelSpreadsheetImport
     			}
     		}
     		
-    		//last: overwrite clause value with body id for later referencing
-    		$clause = $clauseBody->get('id');
+    		//last: overwrite clause value with body id and number for later referencing
+    		$clause = array($clauseBody->get('id') => array($clauseNumber,$clauseParentNumber));
     	}
     	
     	$documentHelper = new DocumentHelper();
     	
     	$newDocument = new Document();
     	
-    	$newDocument->set('name', key($documents));
+    	$newDocument->set('name', $documentName);
     	$newDocument->set('code', $document['code']);
-    	$newDocument->set('adoption_date', $document['data'][4]);
+    	$newDocument->set('adoption_date', $this->createDate($document['data'][4]));
     	$newDocument->set('organisation_id', $documentHelper->retrieveOrganisation($document['data'][5])); //organisation
       $newDocument->set('documenttype_id', $documentHelper->retrieveDocumentType($document['data'][11])); //document type
     	$newDocument->set('document_url', $document['data'][17]);
       
     	$newDocument->save();
     	
-    	//$document = new Document();
+    	$document['id'] = $newDocument->get('id');
     }
-//  	
-//  	clause bodys
-//  	
-//  	clauses
-//  	
-//  	document tags
-//  	
-//  	documents
+
+    // find and save document parent relations
+    foreach($documents as $documentA)
+    {
+    	foreach($documents as $documentB)
+      {
+        $compareResult = strcasecmp($documentA['code'], $documentB['followup']);
+	      if($compareResult == 0 && $documentA['code'] != "")
+	      {
+	      	// Document B is parent of Document A
+	      	$child = Doctrine_Core::getTable('Document')->find($documentA['id']);
+          $child->set('parent_document_id', $documentB['id']);
+          $child->save();
+          
+          // Clause Body Relations
+          foreach($documentA['clauses'] as $keyA => $clauseOfA)
+          {
+          	foreach($documentB['clauses'] as $keyB => $clauseOfB)
+          	{          		
+          		$id_a = key($clauseOfA);
+          		$id_b = key($clauseOfB);
+          		
+          		if($clauseOfA[$id_a][1] == $clauseOfB[$id_b][0] && $clauseOfA[$id_a][1] != "" && $clauseOfB[$id_b][0] != "")
+          		{
+          			// Clause Body B is parent of Clause Body A
+			          $child = Doctrine_Core::getTable('ClauseBody')->find($id_a);
+			          $child->set('parent_clause_body_id', $id_b);
+			          $child->save();
+          		}
+          	}
+          }
+	      }
+      }
+    }
+  }
+  /**
+   * Receives an excel date number (days since 1-1-1900).
+   * Converts it into a timestamp which then is converted into
+   * a date string of format Y-m-d.
+   * Returns NULL when date number is not valid.
+   * 
+   * @param $dateInteger
+   * @return unknown_type
+   */
+  private function createDate($dateInteger)
+  {
+    $dateInteger = trim($dateInteger);
+    $dateInteger = explode(" ", $dateInteger);
+    
+    if($dateInteger[0] > 0 && $dateInteger[0] != "" && !(count($dateInteger) > 1))
+    {
+      $oneDayInSeconds = 24 * 60 * 60; //hours * minutes * seconds
+      $utcDateInExcel = 25569; //number of days since 1-1-1900 until 1-1-1970
+      $utcDateInExcelInSeconds = $utcDateInExcel * $oneDayInSeconds;
+      $excelDateInSeconds = $dateInteger[0] * $oneDayInSeconds;
+      
+      $dateInUtcTimestamp = $excelDateInSeconds - $utcDateInExcelInSeconds;
+      
+      return date("Y-m-d", $dateInUtcTimestamp);
+    }
+    else
+    {
+      return NULL;
+    }
   }
 }

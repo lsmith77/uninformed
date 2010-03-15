89f6ca4b72ca185f78f435a66b0cac52725615d3
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-03-15 18:36:49 +0000

    updated for current schema

--- trunk/lib/filter/doctrine/base/BaseCountryVersionFormFilter.class.php
+++ trunk/lib/filter/doctrine/base/BaseCountryVersionFormFilter.class.php
@@ -13,6 +13,7 @@ abstract class BaseCountryVersionFormFilter extends BaseFormFilterDoctrine
   public function setup()
   {
     $this->setWidgets(array(
+      'iso'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'name'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
@@ -20,6 +21,7 @@ abstract class BaseCountryVersionFormFilter extends BaseFormFilterDoctrine
     ));
 
     $this->setValidators(array(
+      'iso'        => new sfValidatorPass(array('required' => false)),
       'name'       => new sfValidatorPass(array('required' => false)),
       'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
@@ -44,6 +46,7 @@ abstract class BaseCountryVersionFormFilter extends BaseFormFilterDoctrine
   {
     return array(
       'id'         => 'Number',
+      'iso'        => 'Text',
       'name'       => 'Text',
       'created_at' => 'Date',
       'updated_at' => 'Date',

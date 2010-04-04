218d945e4f0341429a2c25186d826e7354008908
riedel.dennis <riedel.dennis@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-04 12:09:41 +0000

    Update on Excel Import. Saves LegalValue DocumentType relation on document import. Refactored documentHelper class.

--- trunk/lib/model/doctrine/LegalValueTable.class.php
+++ trunk/lib/model/doctrine/LegalValueTable.class.php
@@ -2,4 +2,34 @@
 
 class LegalValueTable extends Doctrine_Table
 {
+  public function getAllLegalValues()
+  {
+    $q = $this->createQuery('l');
+    return $q->fetchArray();
+  }
+  
+  public function getAllLegalValueNames()
+  {
+    $q = $this->createQuery('t')
+      ->select('t.name');
+      
+     return $q->fetchArray();
+  }
+  
+  public function retrieveLegalValueIdByName($legalValueName)
+  {
+    $legalValues = $this->getAllLegalValues();
+    
+    foreach($legalValues as $legalValue)
+    {
+      //case insensitive comparison
+      $compareResult = strcasecmp($legalValueName, $legalValue['name']);
+      if($compareResult == 0)
+      {
+        return $legalValue['id'];
+      }
+    }
+    
+    return NULL;
+  }
 }

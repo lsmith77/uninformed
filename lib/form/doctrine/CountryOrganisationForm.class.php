f56692ca5a9fa6f866145dcb80a21813aa71db10
riedel.dennis <riedel.dennis@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-10 21:03:16 +0000

    Initial commit CountryOrganisation admin module. Allows the adminstration of country membership in organisations. Customized form class to allow date range between 1945 and 2045.

--- lib/form/doctrine/CountryOrganisationForm.class.php
+++ lib/form/doctrine/CountryOrganisationForm.class.php
@@ -10,6 +10,16 @@
  */
 class CountryOrganisationForm extends BaseCountryOrganisationForm
 {
+	public function setup()
+	{
+		parent::setup();
+		
+		$years = range(1945, 2045);
+		
+		$this->setWidget('eff_date', new sfWidgetFormDate(array('years' => array_combine($years, $years))));
+    $this->setWidget('exp_date', new sfWidgetFormDate(array('years' => array_combine($years, $years))));
+	}
+	
   public function configure()
   {
   }

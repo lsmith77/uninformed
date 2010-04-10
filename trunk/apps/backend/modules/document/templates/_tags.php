50c155c45793d1ead09121d62af39422fe38f370
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-10 16:33:20 +0000

    added todo note

--- trunk/apps/backend/modules/document/templates/_tags.php
+++ trunk/apps/backend/modules/document/templates/_tags.php
@@ -1,3 +1,4 @@
 <?php
+// TODO: always only show the first line and the rest on a mouseover
 foreach ($document->Tags as $tag)
     echo link_to($tag, @default_edit, array('module' => 'tag', 'id' => $tag->id, 'action' => 'edit')).'<br />'; ?>

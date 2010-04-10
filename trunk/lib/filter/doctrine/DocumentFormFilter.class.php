77c1855198ec9861a3c5c6af25fde2afd8335d7f
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-10 16:33:35 +0000

    fixed autocomplete url

--- trunk/lib/filter/doctrine/DocumentFormFilter.class.php
+++ trunk/lib/filter/doctrine/DocumentFormFilter.class.php
@@ -12,5 +12,17 @@ class DocumentFormFilter extends BaseDocumentFormFilter
 {
   public function configure()
   {
+    parent::configure();
+
+    sfContext::switchTo('backend');
+
+    $this->widgetSchema['code'] = new sfWidgetFormDoctrineJQueryAutocompleter(
+      array(
+        'model' => 'Document',
+        'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=document&action=autocomplete'),
+      )
+    );
+
+    $this->validatorSchema['code'] = new sfValidatorPass ();
   }
 }

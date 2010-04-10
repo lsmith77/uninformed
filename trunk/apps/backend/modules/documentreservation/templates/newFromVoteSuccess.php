54e08e63be9d06023a9597649bc2dc4517593016
riedel.dennis <riedel.dennis@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-10 20:24:26 +0000

    Update on Vote form. Selecting a document filters country list depending on Organisation and the entry and exit dates of the Country in the Organisation. New fixture for country organisation relation for testing purposes. Please use the new dummy Excel posted on the mailing list for testing purposes. Minor files missing from previous commit.

--- trunk/apps/backend/modules/documentreservation/templates/newFromVoteSuccess.php
+++ trunk/apps/backend/modules/documentreservation/templates/newFromVoteSuccess.php
@@ -1,38 +1,24 @@
 <?php use_helper('jQuery'); ?>
 
-<h3>Clause Reservation</h3>
+<h3>Document Reservation</h3>
 
-<p id="clauseReservationFeedback">
+<p id="documentReservationFeedback">
 </p>
 
 <?php echo jq_form_remote_tag(array(
-      'update'   => 'clauseReservationFeedback',
-      'url'      => 'clausereservation/createFromVote',
+      'update'   => 'documentReservationFeedback',
+      'url'      => 'documentreservation/createFromVote',
   )) ?>
 
   <?php echo tag('input', array('name' => 'country_id', 'type' => 'hidden', 'value' => $params['country_id'])) ?>
   <?php echo tag('input', array('name' => 'document_id', 'type' => 'hidden', 'value' => $params['document_id'])) ?>
-  
-  <?php
-  $options = "";
-  
-  foreach($params['clauses'] as $clause)
-  {
-    $options .= '<option value="'.$clause['clause_body_id'].'">'.$clause['slug'].'</option>';
-  }
-  ?>
-  
-  <div>
-    <?php echo content_tag('select', $options, array('name' => 'clause_body_id')) ?>
-  </div>
-  
+
   <div>
 	  <?php echo content_tag('textarea',
 	    '',
 	    array('name' => 'reservationText', 'cols' => '80', 'rows' => '10'))
 	  ?>
   </div>
-  
   <div>
     <?php echo tag('input', array('type' => 'submit')) ?>
   </div>

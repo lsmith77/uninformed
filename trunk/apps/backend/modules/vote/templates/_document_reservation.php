ae348c4fe86a8fb7ca8bbdd568e7147c2d01d9c9
riedel.dennis <riedel.dennis@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-12 20:54:42 +0000

    Update on Vote List view. Applying filter for columns document reservation and clause reservation. Partials check whether vote is of type "ratified" before displaying "add" options for reservations.

--- trunk/apps/backend/modules/vote/templates/_document_reservation.php
+++ trunk/apps/backend/modules/vote/templates/_document_reservation.php
@@ -1,7 +1,14 @@
 <?php
 
-$document = $vote->getDocument();
-$country = $vote->getCountry();
-
-echo link_to("Add", 'documentreservation/new?document='.$document.'&country='.$country)
+if($vote->getType() == "ratified")
+{
+	$document = $vote->getDocument();
+	$country = $vote->getCountry();
+	
+	echo link_to("Add", 'documentreservation/new?document='.$document.'&country='.$country);
+}
+else
+{
+	echo "n/a";
+}
 ?>

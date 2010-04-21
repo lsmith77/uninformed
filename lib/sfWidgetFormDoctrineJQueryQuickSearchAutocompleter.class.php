c879b7c5008f2d5778e9bb3cf828bccab7f69d51
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-04-21 21:05:31 +0000

    minor tweak for two step autocompletion

--- lib/sfWidgetFormDoctrineJQueryQuickSearchAutocompleter.class.php
+++ lib/sfWidgetFormDoctrineJQueryQuickSearchAutocompleter.class.php
@@ -24,7 +24,7 @@ class sfWidgetFormDoctrineJQueryQuickSearchAutocompleter extends sfWidgetFormDoc
     {
       $url = url_for('@default_edit?module='.strtolower($this->getOption('model')).'&action=edit&id=XXX');
       $widget = parent::render($name, '', $attributes, $errors);
-      $widget = str_replace('.val(data[1]);', ".val(data[1]); if (data[1] !== '') { url = '$url'; location.href = url.replace(/XXX/, data[1]); }", $widget);
+      $widget = str_replace('.val(data[1]);', ".val(data[1]); if (data[1].replace(/ /, '') !== '') { url = '$url'; location.href = url.replace(/XXX/, data[1]); }", $widget);
       return $widget;
     }
 }

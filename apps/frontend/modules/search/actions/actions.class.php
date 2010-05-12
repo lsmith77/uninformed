cc76362afca28cc97e56a017556c483b7d70df63
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-12 14:19:34 +0000

    added support for filters, use standard params

--- apps/frontend/modules/search/actions/actions.class.php
+++ apps/frontend/modules/search/actions/actions.class.php
@@ -59,11 +59,14 @@ class searchActions extends sfActions
 
     public function executeResults(sfWebRequest $request)
     {
-        $fulltext = $request->getGetParameter('fulltext');
+        $this->query = $request->getGetParameter('q');
+        $this->tagMatch = $request->getGetParameter('tm');
+        $this->tags = (array)$request->getGetParameter('t');
+        $this->filters = (array)$request->getGetParameter('f');
+
         $lucene = $this->getInstance();
 
         $criteria = new sfLuceneFacetsCriteria;
-        $criteria->addSane($fulltext);
 
         $facets = array(
             'organisation_id' => 'Organisation',
@@ -80,7 +83,40 @@ class searchActions extends sfActions
             $criteria->addFacetField($facet);
         }
 
+        if (!empty($this->filters)) {
+            $criteria2 = new sfLuceneCriteria;
+        }
+
+        if (!empty($this->query)) {
+            $criteria->addSane($this->query);
+            if (isset($criteria2)) {
+                $criteria2->addSane($this->query);
+            }
+        }
+        if (!empty($this->tags)) {
+            // TODO: add validation to ensure we are only getting integers
+            $fq_op = ($this->tagMatch == 'all') ? ' AND ' : ' OR ';
+            $fq = 'tag_ids:'.implode($fq_op.'tag_ids:', $this->tags);
+            $criteria->addParam('fq', $fq);
+            if (isset($criteria2)) {
+                $criteria2->addParam('fq', $fq);
+            }
+        }
+
+        // TODO: handle case where both query and tags is empty
+
+        if (isset($criteria2)) {
+            $criteria->setLimit(0);
+            $fq = isset($fq) ? "($fq) AND " : '';
+            foreach ($this->filters as $filter => $ids) {
+                // TODO: add validation to ensure we are handle supported $facets
+                $fq.= $filter.':'.implode(' AND '.$filter.':', $ids);
+            }
+            $criteria2->addParam('fq', $fq);
+        }
+
         $results = $lucene->friendlyFind($criteria);
+
         $filters = array();
         foreach ($facets as $facet => $model) {
             $solr = $results->getFacetField($facet);
@@ -105,7 +141,9 @@ class searchActions extends sfActions
             }
         }
 
-        $data = $lucene->friendlyFind($criteria)->toArray();
+        $data = isset($criteria2)
+            ? $lucene->friendlyFind($criteria2)->toArray()
+            : $results->toArray();
 
         $output = array('data' => $data, 'filters' => $filters);
         return $this->returnJson($output);

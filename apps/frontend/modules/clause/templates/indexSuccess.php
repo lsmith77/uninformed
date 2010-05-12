ebd7bc535d32ed975353e32513d8f2befb8f40fc
ebert86@gmail.com <ebert86@gmail.com@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-12 12:40:01 +0000

    Added basic clause content on index action and created the clause list partial

--- apps/frontend/modules/clause/templates/indexSuccess.php
+++ apps/frontend/modules/clause/templates/indexSuccess.php
@@ -1,3 +1,61 @@
 <?php echo $clause ?>
 <br />
-<?php echo $clause->ClauseBody->getContent() ?>
+
+<h2>Content</h2>
+<div><?php echo $clauseBody->getContent() ?></div>
+
+<h2>Clause Details</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Clause Number</th>
+            <th>Operative Phrase</th>
+            <th>Type of Information</th>
+            <th>Addresses</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $clause->clause_number; ?></td>
+            <td><?php echo $clauseBody->ClauseOperativePhrase; ?></td>
+            <td><?php echo $clauseBody->ClauseInformationType; ?></td>
+            <td>
+            <?php foreach($clauseBody->Addressees as $address): ?>
+            <?php echo $address->getName(); ?><br />
+            <?php endforeach; ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+
+<h2>Applied Keyword Tags</h2>
+<div>
+    <?php foreach($clauseBody->Tags as $tag): ?>
+    <?php //TODO: change link url! ?>
+    <?php echo link_to($tag->getName(), $clause); ?>
+    <?php endforeach; ?>
+</div>
+
+
+<h2>History of this Clause</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Year</th>
+            <th>Changes</th>
+            <th>Content</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+
+<h2>Other Clauses in this Document</h2>
+<?php include_partial('clauseListOfDocument', array('clauses' => $document->getClauses(), 'currentClause' => $clause)) ?>

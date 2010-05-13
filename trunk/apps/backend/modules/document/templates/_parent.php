946f22e53df937727c7503d2dd91109fbf6dab33
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-13 09:30:19 +0000

    fixed parent handling

--- trunk/apps/backend/modules/document/templates/_parent.php
+++ trunk/apps/backend/modules/document/templates/_parent.php
@@ -1,1 +1,1 @@
-<?php if ($document->parent_document_id) echo link_to($document->Parent, '@default_edit?module=document&action=edit&id='.$document->parent_document_id); ?>
+<?php if ($document->parent_document_id) echo link_to($document->DocumentParent, '@default_edit?module=document&action=edit&id='.$document->parent_document_id); ?>

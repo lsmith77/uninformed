34677c69ac3d777374d4dce5f8fb91dfa7b84534
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-16 15:06:26 +0000

    added admintools for tags and relations (still need to be tweaked)

---
+++ apps/backend/modules/document_clause_relation/lib/document_clause_relationGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * document_clause_relation module configuration.
+ *
+ * @package    symfony
+ * @subpackage document_clause_relation
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class document_clause_relationGeneratorConfiguration extends BaseDocument_clause_relationGeneratorConfiguration
+{
+}
---
+++ apps/backend/modules/document_document_relation/lib/document_document_relationGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * document_document_relation module helper.
+ *
+ * @package    symfony
+ * @subpackage document_document_relation
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class document_document_relationGeneratorHelper extends BaseDocument_document_relationGeneratorHelper
+{
+}

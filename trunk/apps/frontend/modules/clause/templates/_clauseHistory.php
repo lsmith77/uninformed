3ff2d43adcdd339e51f19e88f430475b2f628ead
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-16 10:42:06 +0000

    updated to current requirements

--- trunk/apps/frontend/modules/clause/templates/_clauseHistory.php
+++ trunk/apps/frontend/modules/clause/templates/_clauseHistory.php
@@ -3,14 +3,15 @@
  * Clause history partial
  *
  * @param     $clause
- * 
+ *
  */
 $rootclauses = $clause->getClausesByRoot();
 $rootdocuments = $clause->Document->getDocumentsByRoot();
 ?>
-<table id="history" class="collapsed">
+<table id="clauseHistory" class="collapsed">
     <thead>
         <tr>
+            <th>Code</th>
             <th>Year</th>
             <th>Changes</th>
             <th>Content</th>
@@ -18,30 +19,41 @@ $rootdocuments = $clause->Document->getDocumentsByRoot();
     </thead>
     <tbody>
         <?php $introduced = null; ?>
-        <?php foreach($rootdocuments as $rootdoc): ?>
+        <?php foreach($rootdocuments as $i => $rootdoc): ?>
         <?php $current = (isset($rootclauses[$rootdoc->getId()])) ? $rootclauses[$rootdoc->getId()] : null; ?>
-        <tr>
+        <tr<?php if ($rootclauses[$rootdoc->getId()]->getId() == $clause->getId()) { echo ' style="background-color: grey"'; } ?>>
+            <td>
+            <?php echo link_to($rootdoc->getCode(), 'document', array('id' => $rootdoc->getSlug())); ?>
+            </td>
             <td>
                 <?php echo date('Y',strtotime($rootdoc->getAdoptionDate())); ?>
             </td>
             <td>
+                <ul>
                 <?php
+                if (!$i) {
+                   echo '<li>Document adopted</li>';
+                }
                 if ($current && $introduced) {
                     $iID = $introduced->ClauseBody->getId();
                     $cID = $current->ClauseBody->getId();
                     if ($iID===$cID) {
-                        echo 'None';
+                        echo '<li>Clause copied</li>';
                     } else {
-                        echo 'Yes';
+                        echo '<li>Clause changed</li>';
                     }
-                }
-                if ($current && !$introduced) {
+                } elseif ($current && !$introduced) {
                    $introduced = $current;
-                   echo 'Clause introduced';
+                   echo '<li>Clause introduced</li>';
+                } elseif (!$current && $introduced) {
+                   $introduced = $current;
+                   echo '<li>Clause removed</li>';
                 }
                 ?>
+                    </ul>
             </td>
             <td>
+                <?php echo link_to($current->getClauseNumber(), 'clause', array('id' => $current->getSlug())); ?>:
                 <?php echo isset($current) ? $current->ClauseBody->getContent() : ''; ?>
             </td>
         </tr>

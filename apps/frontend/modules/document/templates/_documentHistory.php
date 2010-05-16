648c4e42bee550c9bbcda1b56bb5e373a3baa7c2
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-16 10:42:06 +0000

    updated to current requirements

--- apps/frontend/modules/document/templates/_documentHistory.php
+++ apps/frontend/modules/document/templates/_documentHistory.php
@@ -7,16 +7,20 @@
  */
 $rootdocuments = $document->getDocumentsByRoot();
 ?>
-<table id="history" class="collapsed">
+<table id="documentHistory" class="collapsed">
     <thead>
         <tr>
+            <th>Code</th>
             <th>Year</th>
             <th>Content</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach($rootdocuments as $rootdoc): ?>
-        <tr>
+        <tr<?php if ($rootdoc->getId() == $document->getId()) { echo ' style="background-color: grey"'; } ?>>
+            <td>
+                <?php echo link_to($rootdoc->getCode(), 'document', array('id' => $rootdoc->getSlug())); ?>
+            </td>
             <td>
                 <?php echo date('Y',strtotime($rootdoc->getAdoptionDate())); ?>
             </td>

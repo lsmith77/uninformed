b14f30693c9b56de4cbec97d3262d607e97cc7de
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-16 13:04:47 +0000

    tweaked bookmark list display

--- apps/frontend/modules/bookmark/templates/indexSuccess.php
+++ apps/frontend/modules/bookmark/templates/indexSuccess.php
@@ -1,9 +1,23 @@
 <h1>Bookmarks</h1>
-<ol class="bookmarks">
-    <?php foreach($bookmarks as $bookmark): ?>
+<ul class="bookmarks">
+    <?php foreach($documents as $document): ?>
     <li>
-        <?php $linkLabel = $bookmark['type'].' #'.$bookmark['id']; ?>
-        <?php echo link_to($linkLabel, $bookmark['model'], array('id'=>$bookmark['id'])); ?>
+        <?php $orgs = $document->getStructuredOrganisation(); ?>
+        <?php if (isset($document_ids[$document->getId()])): ?>
+            <?php echo link_to($document->getName(), 'document', array('id' => $document->getSlug())); ?>
+        <?php else: ?>
+        <?php echo $document->getName(); ?>
+        <?php endif; ?>
+        (<?php echo $orgs['main'].' '.$orgs['current'].' '.$orgs['sub']; ?>)
+        <ul class="bookmarks">
+            <?php foreach($document->Clauses as $clause): ?>
+            <?php if (isset($clause_ids[$clause->getId()])): ?>
+            <li>
+                -> <?php echo link_to($clause->getClauseNumber(), 'clause', array('id' => $clause->getSlug())); ?>
+            </li>
+            <?php endif; ?>
+            <?php endforeach; ?>
+        </ul>
     </li>
     <?php endforeach; ?>
-</ol>
+</ul>

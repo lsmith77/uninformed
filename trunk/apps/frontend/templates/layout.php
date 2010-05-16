127dfdbb6547e6f5d3aa1d6575b4351605c638bf
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-16 13:48:55 +0000

    added GA code

--- trunk/apps/frontend/templates/layout.php
+++ trunk/apps/frontend/templates/layout.php
@@ -9,6 +9,22 @@
     <?php include_javascripts() ?>
   </head>
   <body>
+<script type="text/javascript">
+
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-16437566-1']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type =
+'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
+'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+ })();
+
+</script>
     <?php if($sf_user->hasFlash('notice')): ?>
       <div id="flash_notice"><b>Notice:</b> <?php echo $sf_user->getFlash('notice'); ?></div>
     <?php endif; ?>

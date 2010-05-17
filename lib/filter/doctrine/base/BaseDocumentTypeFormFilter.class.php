e7996332b6fceb55cf98c56ae3c46a3289fd757f
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-17 18:30:09 +0000

    made legal value a varchar(30)

--- lib/filter/doctrine/base/BaseDocumentTypeFormFilter.class.php
+++ lib/filter/doctrine/base/BaseDocumentTypeFormFilter.class.php
@@ -15,7 +15,7 @@ abstract class BaseDocumentTypeFormFilter extends BaseFormFilterDoctrine
     $this->setWidgets(array(
       'name'          => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'rank_priority' => new sfWidgetFormFilterInput(),
-      'legal_value'   => new sfWidgetFormChoice(array('choices' => array('' => '', 'legally binding' => 'legally binding', 'non-legally binding' => 'non-legally binding', 'support document' => 'support document'))),
+      'legal_value'   => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'created_at'    => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at'    => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'author_id'     => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Author'), 'add_empty' => true)),
@@ -24,7 +24,7 @@ abstract class BaseDocumentTypeFormFilter extends BaseFormFilterDoctrine
     $this->setValidators(array(
       'name'          => new sfValidatorPass(array('required' => false)),
       'rank_priority' => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'legal_value'   => new sfValidatorChoice(array('required' => false, 'choices' => array('legally binding' => 'legally binding', 'non-legally binding' => 'non-legally binding', 'support document' => 'support document'))),
+      'legal_value'   => new sfValidatorPass(array('required' => false)),
       'created_at'    => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at'    => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'author_id'     => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Author'), 'column' => 'id')),
@@ -50,7 +50,7 @@ abstract class BaseDocumentTypeFormFilter extends BaseFormFilterDoctrine
       'id'            => 'Number',
       'name'          => 'Text',
       'rank_priority' => 'Number',
-      'legal_value'   => 'Enum',
+      'legal_value'   => 'Text',
       'created_at'    => 'Date',
       'updated_at'    => 'Date',
       'author_id'     => 'ForeignKey',

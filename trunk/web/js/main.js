a4d27a9439cc845a9be3e1a2126bdadfcbe93e8f
boggiano.jordi@gmail.com <boggiano.jordi@gmail.com@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-19 11:02:51 +0000

    fixes #31

--- trunk/web/js/main.js
+++ trunk/web/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
 });
+
+(function($) {
+    $.extend({
+        urlencode: function(text) {
+            return encodeURIComponent(text);
+        },
+        htmlencode: function(text) {
+            var findReplace = [[/&/g, "&amp;"], [/</g, "&lt;"], [/>/g, "&gt;"], [/"/g, "&quot;"]];
+            for (idx in findReplace) {
+                text = text.replace(findReplace[idx][0], findReplace[idx][1]);
+            }
+            return text;
+        }
+    });
+})(jQuery);

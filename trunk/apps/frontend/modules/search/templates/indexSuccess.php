8c655713fcf41976659bf073c3fa0499447ad6ce
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-24 14:27:41 +0000

    added note about the current covered areas

--- trunk/apps/frontend/modules/search/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/search/templates/indexSuccess.php
@@ -4,5 +4,7 @@
 <h1>Search for clauses</h1>
 
 <div class="homesearch">
+<div>Currently limited to the areas of small arms and light weapons, women and education, clean drinking water and malaria.</div>
+<br />
 <?php include_partial('search/searchForm', $sf_data); ?>
 </div>

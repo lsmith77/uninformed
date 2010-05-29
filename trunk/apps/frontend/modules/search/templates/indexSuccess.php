61b901e565ba259ea468403e303f1842c970ed9e
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-29 12:43:58 +0000

    expanded title to be more descriptive

--- trunk/apps/frontend/modules/search/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/search/templates/indexSuccess.php
@@ -1,7 +1,7 @@
 <?php
     use_javascript('search');
 ?>
-<h1>Search for clauses</h1>
+<h1>Search clauses of UN resolutions and conventions</h1>
 
 <div class="homesearch">
 <div>Currently limited to the areas of small arms and light weapons, women and education, clean drinking water and malaria.</div>

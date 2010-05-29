4fae27685bf248e480a5e482656aaaa718407856
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-05-29 22:00:52 +0000

    text tweak

--- apps/frontend/modules/search/templates/indexSuccess.php
+++ apps/frontend/modules/search/templates/indexSuccess.php
@@ -6,5 +6,5 @@
 <div class="homesearch">
     <?php include_partial('search/searchForm', $sf_data); ?>
 
-    <span class="helptext">Currently limited to the areas of small arms and light weapons, women and education, clean drinking water and malaria.</span>
+    <span class="helptext">Searches are currently limited to the areas of small arms and light weapons, women and education, clean drinking water and malaria.</span>
 </div>

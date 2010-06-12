e7029f34883233924212eba17c703d070b294f33
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2010-06-12 16:32:36 +0000

    improved help text

--- apps/frontend/modules/search/templates/indexSuccess.php
+++ apps/frontend/modules/search/templates/indexSuccess.php
@@ -5,8 +5,14 @@
 
 <div class="homesearch">
     <fieldset>
-        <legend>Note</legend>
-        <span>Searches are currently limited to the areas of small arms and light weapons, women and education, clean drinking water and malaria.</span>
+        <legend>Notes:</legend>
+        <ul>
+            <li>Please use the below form to enter search criteria. On the following page additional
+        filters can be set to further narrow down the results shown.</li>
+            <li>Searches are currently limited to the areas of small arms and light weapons, women and education, clean
+        drinking water and malaria.</li>
+            <li>The database currently only contains documents and clauses negotiated by states.</li>
+        </ul>
     </fieldset>
 
     <br />

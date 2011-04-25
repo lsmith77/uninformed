0fa6f5f8e4a0b049e2e2bec3a9a6f8bc59f7ec03
lukas.smith <lukas.smith@cb9bc1ea-8f18-11de-9a8c-6d5b7d1e4d13>
2011-04-25 13:13:11 +0000

    WS tweaks

--- branches/unified-search-js/lib/form/doctrine/DocumentForm.class.php
+++ branches/unified-search-js/lib/form/doctrine/DocumentForm.class.php
@@ -10,39 +10,41 @@
  */
 class DocumentForm extends BaseDocumentForm
 {
-  public function setup()
-  {
-    parent::setup();
-    if ($this->isNew()) {
-      unset($this->widgetSchema['slug']);
-    } else {
-      $this->setWidget('slug', new sfWidgetFormPlain(array('value'=>$this->getObject()->slug)));
+    public function setup()
+    {
+        parent::setup();
+        if ($this->isNew()) {
+            unset($this->widgetSchema['slug']);
+        } else {
+            $this->setWidget('slug', new sfWidgetFormPlain(array('value'=>$this->getObject()->slug)));
+        }
     }
-  }
-    public function configure() {
+
+    public function configure()
+    {
         $years = range(1900, date('Y'));
         $dateWidget = new sfWidgetFormDate(array('years' => array_combine($years, $years)));
         $this->widgetSchema['enforcement_date'] = new sfWidgetFormJQueryDate(array('date_widget' => $dateWidget));
         $this->widgetSchema['adoption_date'] = new sfWidgetFormJQueryDate(array('date_widget' => $dateWidget));
         $this->widgetSchema['parent_document_id'] = new sfWidgetFormDoctrineJQueryChoiceAutocompleter(
-          array(
-            'model' => 'Document',
-            'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=document&action=autocomplete'),
-          )
+            array(
+                'model' => 'Document',
+                'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=document&action=autocomplete'),
+            )
         );
         $this->widgetSchema['organisation_id'] = new sfWidgetFormDoctrineJQueryChoiceAutocompleter(
-          array(
-            'model' => 'Organisation',
-            'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=organisation&action=autocomplete'),
+            array(
+                'model' => 'Organisation',
+                'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=organisation&action=autocomplete'),
           )
         );
         $this->widgetSchema['tags_list'] = new sfWidgetFormDoctrineJQueryChoiceAutocompleter(
-          array(
-            'renderer_class' => 'sfWidgetFormSelectDoubleList',
-            'model' => 'Tag',
-            'multiple' => true,
-            'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=tag&action=autocomplete'),
-          )
+            array(
+                'renderer_class' => 'sfWidgetFormSelectDoubleList',
+                'model' => 'Tag',
+                'multiple' => true,
+                'url'   => sfContext::getInstance()->getController()->genUrl('@default?module=tag&action=autocomplete'),
+            )
         );
     }
 }
